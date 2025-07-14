487164eaa4ebee9fbc1c7bb75964dba41185a497
Hruday Chowdary Gurijala <gurijalahruday@gmail.com>
2025-07-14 13:32:37 +0530

    setup env vars and mongodb

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,12 @@
 import express from "express"
+import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
 
 const app = express();
+connectDB()
+const PORT = ENV.PORT
 
 
-const PORT = 42069;
+app.get("/",(req,res)=> res.send("testing"))
 
 app.listen(PORT,()=>console.log(`server at http://localhost:${PORT}`))
