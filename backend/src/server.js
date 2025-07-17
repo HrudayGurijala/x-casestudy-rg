4a2c3bc87115ba72d871b9016caf7660b5bb8e8d
Hruday Chowdary Gurijala <gurijalahruday@gmail.com>
2025-07-17 11:17:34 +0530

    notif handling and endpoints

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,7 @@ import {clerkMiddleware} from "@clerk/express"
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js"
+import notificationRoutes from "./routes/notification.route.js"
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 
@@ -20,6 +21,7 @@ app.get("/",(req,res)=> res.send("testing"))
 app.use("api/users",userRoutes)
 app.use("api/posts",postRoutes)
 app.use("api/comments",commentRoutes)
+app.use("api/notifications",notificationRoutes)
 
 const startServer = async () => {
     try {
