8bdd453b316dc209a87706684630c08546869876
Hruday Chowdary Gurijala <gurijalahruday@gmail.com>
2025-07-17 11:24:03 +0530

    arcjet config and defined middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,6 +8,7 @@ import commentRoutes from "./routes/comment.route.js"
 import notificationRoutes from "./routes/notification.route.js"
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
+import { arcjetMiddleware } from "./middlewares/arcjet.middleware.js";
 
 const app = express();
 const PORT = ENV.PORT
@@ -15,6 +16,7 @@ const PORT = ENV.PORT
 app.use(cors())
 app.use(express.json())
 app.use(clerkMiddleware())
+app.use(arcjetMiddleware)
 
 app.get("/",(req,res)=> res.send("testing"))
 
