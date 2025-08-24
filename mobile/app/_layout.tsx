4267270d69b192b837c674a53095a969f1abe152
Hruday Chowdary Gurijala <gurijalahruday@gmail.com>
2025-08-25 01:01:23 +0530

    fix: used diff auth route handling

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,20 +1,40 @@
-import {ClerkProvider} from "@clerk/clerk-expo"
+import {ClerkProvider,useAuth} from "@clerk/clerk-expo"
 import { tokenCache } from '@clerk/clerk-expo/token-cache'
 import { Stack } from "expo-router";
 import "../global.css"
 import { QueryClient,QueryClientProvider} from "@tanstack/react-query"
+import {  View } from "react-native";
+import { Feather } from "@expo/vector-icons";
 
 const queryClient = new QueryClient();
 
+function RootLayoutNav() {
+  const { isSignedIn, isLoaded } = useAuth();
+
+  if (!isLoaded) {
+    return (
+      <View className="flex-1 justify-center items-center">
+        <Feather name="twitter" size={100} color="#1DA1F2" />
+      </View>
+    );
+  }
+
+  return (
+    <Stack screenOptions={{headerShown: false}}>
+      {!isSignedIn ? (
+        <Stack.Screen name="(auth)" />
+      ) : (
+        <Stack.Screen name="(tabs)" />
+      )}
+    </Stack>
+  );
+}
 
 export default function RootLayout() {
   return (
     <ClerkProvider tokenCache={tokenCache}>
       <QueryClientProvider client ={queryClient}>
-      <Stack screenOptions={{headerShown: false}}>
-        <Stack.Screen name="(auth)" />
-        <Stack.Screen name="(tabs)" />
-      </Stack>
+        <RootLayoutNav />
       </QueryClientProvider>
     </ClerkProvider>
   );
